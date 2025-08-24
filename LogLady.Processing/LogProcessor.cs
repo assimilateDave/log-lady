180701ffdf12a251429f3612dd0e7b3a83fc6068
assimilateDave <165820095+assimilateDave@users.noreply.github.com>
2025-08-23 19:46:56 -0700

    Add log processor class

---
+++ LogLady.Processing/LogProcessor.cs
@@ -0,0 +1,12 @@
+using LogLady.Data;
+
+namespace LogLady.Processing
+{
+    public class LogProcessor
+    {
+        public void Process(string filePath)
+        {
+            // TODO: Implement log file processing logic
+        }
+    }
+}
