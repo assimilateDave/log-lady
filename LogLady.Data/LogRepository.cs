1fe9214b025dc6695af27e0a48dda8f297409013
assimilateDave <165820095+assimilateDave@users.noreply.github.com>
2025-08-23 19:46:49 -0700

    Add log repository class in repository assimilateDave/log-lady

---
+++ LogLady.Data/LogRepository.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LogLady.Data
+{
+    public class LogRepository
+    {
+        public void SaveLog(string message)
+        {
+            // TODO: Implement saving log message to DB
+        }
+    }
+}
