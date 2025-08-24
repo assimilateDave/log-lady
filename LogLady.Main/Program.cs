e61afa471a8ac7cf56e4b87e55c80d654944a8a1
assimilateDave <165820095+assimilateDave@users.noreply.github.com>
2025-08-23 19:47:06 -0700

    Add main program entry point

---
+++ LogLady.Main/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace LogLady.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LogLady!");
+            // TODO: Wire up LogLady.Processing and LogLady.Data in this entrypoint.
+        }
+    }
+}
